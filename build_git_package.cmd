b08f741b31eb3cad24e674be4fa959069cf7dd0a
olgae <olga.e@qualisystems.com>
2016-02-16 18:27:31 +0200

    minor fix for build_git_package

--- build_git_package.cmd
+++ build_git_package.cmd
@@ -1,7 +1,8 @@
 #@ECHO OFF
+pushd ..\%1
 IF EXIST dist RMDIR /q /s dist
-IF EXIST %1-1.0.0 RMDIR /q /s %1-1.0.0
-IF EXIST %1.egg-info RMDIR /q /s %1.egg-info
+IF EXIST %2-1.0.0 RMDIR /q /s %2-1.0.0
+IF EXIST %2.egg-info RMDIR /q /s %2.egg-info
 
 %programdata%\qualisystems\qspython27\python setup.py sdist --format zip
 
