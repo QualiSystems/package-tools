1cb186f15e14aaff11655260cf9b58e1d53dd707
olgae <olga.e@qualisystems.com>
2016-03-02 12:31:54 +0200

    minor

---
+++ download_dependencies.cmd
@@ -0,0 +1,11 @@
+#pushd ..\%1
+
+cd %1
+dir
+
+IF EXIST dependencies RMDIR /q /s dependencies
+MKDIR dependencies
+
+%programdata%\qualisystems\qspython27\Scripts\pip download -r requirements.txt -d dependencies
+
+#popd
