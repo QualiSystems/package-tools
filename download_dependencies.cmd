fbb97f5da9056662c8721a0ed6d67abec7ba7788
olgae <olga.e@qualisystems.com>
2016-03-02 11:45:27 +0200

    initial add

---
+++ download_dependencies.cmd
@@ -0,0 +1,8 @@
+@ECHO OFF
+pushd ..\%1
+IF EXIST dependencies RMDIR /q /s dependencies
+MKDIR dependencies
+
+%programdata%\qualisystems\qspython27\Scripts\pip install --download dependencies\ dist\%2.zip
+
+popd
