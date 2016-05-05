8923e12e3955b0e1753e7bc9296d690e4685c5bc
olgae <olga.e@qualisystems.com>
2016-05-05 16:41:52 +0300

    minor

--- build_git_package.cmd
+++ build_git_package.cmd
@@ -3,9 +3,9 @@ echo %1
 for %%X in (python.exe) do (set PythonPath=%%~$PATH:X)
 IF NOT DEFINED PythonPath GOTO PythonMissing
 
-IF %1 == 6.4 (echo "##teamcity[setParameter name='global.package.version.prefix' value='1.0'] "")
+IF %1 == 6.4 (echo "##teamcity[setParameter name='global.package.version.prefix' value='1.0']")
 IF %1 == dev (echo "##teamcity[setParameter name='global.package.version.prefix' value='2.0']")
-IF %1 == dev_7.1 (echo "##teamcity[setParameter name='global.package.version.prefix' value='2.1'] "") else (echo %1)
+IF %1 == dev_7.1 (echo "##teamcity[setParameter name='global.package.version.prefix' value='2.1']") else (echo %1)
 
 
 IF EXIST dist RMDIR /q /s dist
