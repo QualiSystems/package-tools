671b58667e16cc185eb03572ff7a49286440cd01
olgae <olga.e@qualisystems.com>
2016-05-05 17:11:24 +0300

    remove version determination

--- build_git_package.cmd
+++ build_git_package.cmd
@@ -3,10 +3,6 @@ echo %1
 for %%X in (python.exe) do (set PythonPath=%%~$PATH:X)
 IF NOT DEFINED PythonPath GOTO PythonMissing
 
-IF %1 == 6.4 (echo "##teamcity[setParameter name='global.package.version.prefix' value='1.0']")
-IF %1 == dev (echo "##teamcity[setParameter name='global.package.version.prefix' value='2.0']")
-IF %1 == dev_7.1 (echo "##teamcity[setParameter name='global.package.version.prefix' value='2.1']") else (echo %1)
-
 
 IF EXIST dist RMDIR /q /s dist
 IF EXIST %2-1.0.0 RMDIR /q /s %2-1.0.0
@@ -22,5 +18,4 @@ ECHO Python is missing
 EXIT /B 1
 
 :end
-
 
