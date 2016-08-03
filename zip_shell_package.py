679394c7e52a8af70d0c6cb0cda78c9b7cd9d2c4
olgae <olga.e@qualisystems.com>
2016-08-03 14:09:08 +0300

    add debug info in zip_shell_package.py

--- zip_shell_package.py
+++ zip_shell_package.py
@@ -2,7 +2,7 @@ import sys
 import os
 import zipfile
 import shutil
-
+import traceback
 
 def main():
     print "ARGS: {}".format(sys.argv)
@@ -27,16 +27,24 @@ def main():
     zip_name = os.path.join(driver_path, '..', driver_name)
     zf = zipfile.ZipFile(zip_name, mode='w')
 
+    print 'Packaging \'{}\': '.format(os.listdir(driver_path))
+    print os.listdir(driver_path)
+
     try:
         for file_name in os.listdir(driver_path):
             file_path = os.path.join(driver_path, file_name)
             zf.write(file_path, file_name)
+
+    except Exception, e:
+        print traceback.format_exc()
     finally:
         zf.close()
 
+    print 'Done.'
     # remove folder with driver data for the archive
+    print 'Remove path {}'.format(driver_path)
     shutil.rmtree(driver_path)
-
+    print 'Done.'
 
 if __name__ == "__main__":
     main()
